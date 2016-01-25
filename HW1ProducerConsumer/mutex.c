1d198cc8a83d26deb33fea12400a561057a6508d
johnhanson <jchanson@cpp.edu>
2016-01-25 14:36:45 -0800

    Branched to my version of the problem with mutexes
    Hopefully I got this branch thing right the second time.

--- HW1ProducerConsumer/mutex.c
+++ HW1ProducerConsumer/mutex.c
@@ -1,66 +1,68 @@
 #include <stdio.h>
 #include <pthread.h>
 
-#define MAX 10000
+// how many numbers should be printed?
+#define MAX 100000
 pthread_mutex_t mutex;
 pthread_cond_t condc, condp;
 int buffer = 0;
 
-void* producer(void* pointer);
-void* consumer(void* pointer);
-
-int main(){
-    pthread_t consumer_thread,producer_thread;
-    pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&condc, NULL);
-    pthread_cond_init(&condp, NULL);
-    pthread_create(&consumer_thread, NULL, consumer, NULL);
-    pthread_create(&producer_thread, NULL, producer, NULL);
-    pthread_join(producer_thread, NULL);
-    pthread_join(consumer_thread, NULL);
-    pthread_cond_destroy(&condc);
-    pthread_cond_destroy(&condp);
-    pthread_mutex_destroy(&mutex);
-
+void *consumer(void *p) {
+	int i;
+	for(i=1; i<=MAX; i++) {
+		// lock the mutex
+		pthread_mutex_lock(&mutex);
+		// wait if the buffer is empty
+		while(buffer == 0) 
+			pthread_cond_wait(&condc, &mutex);
+		buffer = buffer - 1;
+		// print out that the consumer consumed!
+		printf("consumer set buffer to %d\n", buffer);
+		// signal the producer
+		pthread_cond_signal(&condp);
+		// release the mutex
+		pthread_mutex_unlock(&mutex);
+	}
+	pthread_exit(0);
 }
 
-
-void* producer(void* pointer){
-    int i;
-    for (i = 1; i <= MAX; i++) {
-        // gain exclusive access to buffer variable
-        pthread_mutex_lock(&mutex);
-        while (buffer != 0) {
-            pthread_cond_wait(&condp, &mutex);
-        }
-
-        //put item in buffer
-        buffer = i;
-        printf("Producer placed %d into buffer\n", buffer);
-
-
-        pthread_cond_signal(&condc);
-        pthread_mutex_unlock(&mutex);
-    }
-    pthread_exit(0);
+void *producer(void *p) {
+	int i;
+	for(i=1; i<=MAX; i++) {
+		// lock the mutex
+		pthread_mutex_lock(&mutex);
+		// wait if there is something in the buffer
+		while(buffer != 0)
+			pthread_cond_wait(&condp, &mutex);
+		buffer = buffer + 1;
+		// print out that something happened!
+		printf("producer set buffer to %d\n",buffer);
+		// wake up the consumer
+		pthread_cond_signal(&condc);
+		// release the mutex
+		pthread_mutex_unlock(&mutex);
+	}
+	pthread_exit(0);
 }
 
-void* consumer(void* pointer){
-    int i;
-    for (i = 1; i <= MAX; i++) {
-        // gain exclusive access to buffer variable
-        pthread_mutex_lock(&mutex);
-        while (buffer == 0) {
-            pthread_cond_wait(&condc, &mutex);
-        }
-
-        printf("Consumer took %d from the buffer\n", buffer);
-        //take item out of buffer
-        buffer = 0;
-
-
-        pthread_cond_signal(&condp);
-        pthread_mutex_unlock(&mutex);
-    }
-    pthread_exit(0);
+int main(int argc, char** argv) {
+	// the two threads, producer and consumer
+	pthread_t prod, cons;
+	// initialize the mutex
+	pthread_mutex_init(&mutex, 0);
+	// init the conditions to wake up
+	pthread_cond_init(&condc, 0);
+	pthread_cond_init(&condp, 0);
+	// create the threads
+	pthread_create(&prod, 0, producer, 0);
+	pthread_create(&cons, 0, consumer, 0);
+	// join them so if one terminates, the other does too
+	pthread_join(prod, 0);
+	pthread_join(cons, 0);
+	// destroy the threads and the mutex
+	pthread_cond_destroy(&condc);
+	pthread_cond_destroy(&condp);
+	pthread_mutex_destroy(&mutex);
+	// exit gracefully
+	return(0);
 }
